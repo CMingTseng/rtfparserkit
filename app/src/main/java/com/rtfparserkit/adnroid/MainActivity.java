1e7cac75ee8004248b1523bbca2ca75b1710b83a
Neo <c.min.tseng@gmail.com>
2018-11-07 11:47:32 +0800

    first commit

---
+++ app/src/main/java/com/rtfparserkit/adnroid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.rtfparserkit.adnroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import com.rtfparserkit.converter.text.StringTextConverter;
+import com.rtfparserkit.parser.RtfStreamSource;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView test = findViewById(R.id.test);
+        try {
+            StringTextConverter converter = new StringTextConverter();
+            converter.convert(new RtfStreamSource(getAssets().open("rtf_test.rtf")));
+            test.setText(converter.getText());
+        } catch (IOException e) {
+
+        }
+    }
+}
